c9632754683e60bd0e30f8ddf56f27823fca8191
Oleg Ivanov <olegivanov1989@gmail.com>
2023-05-22 20:27:49 +0300

    Add wait to main window

--- fixture/application.py
+++ fixture/application.py
@@ -6,6 +6,7 @@ class Application:
     def __init__(self, target):
         self.application = WinApplication(backend="win32").start(target)
         self.main_window = self.application.window(title="Free Address Book")
+        self.main_window.wait("visible")
 
     def destroy(self):
         self.main_window.close()
